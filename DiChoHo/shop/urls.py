73174922807be0c25b903889a8606f2ef332cc2b
QuocHuy <mr.huy7122001@gmail.com>
2021-10-26 17:57:45 +0700

    Translate some page from English to Vietnamese

--- DiChoHo/shop/urls.py
+++ DiChoHo/shop/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from shop import views
 
 urlpatterns = [
-    path('', views.index_view, name='home'), # khi goi duong dan home thi se render html minh lien ket ben views.py, name dung de goi duong dan cho nhanh
+    # khi goi duong dan home thi se render html minh lien ket ben views.py, name dung de goi duong dan cho nhanh
+    path('', views.index_view, name='home'),
     path('shop/', views.shop_view, name='shop'),
     path('product/', views.product_view, name='product'),
     path('login/', views.login_view, name='login'),
